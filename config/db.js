29dc231706b51928f4daceecc1f5a581d67ec02d
unknown <tehminasaleem13@gmail.com>
2024-05-21 08:14:31 -0700

    update: updated index file and put db connection string in dotenv

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
-// config/db.js
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://breakcomp:BreakComp6621@breakcomp.b7ooyku.mongodb.net/myapp",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require("express");
-// const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 const corsMiddleware = require("./config/cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Call the connectDB function to establish the connection
 connectDB();
 
 // Middleware
-// Apply CORS middleware
 app.use(corsMiddleware);
 app.use(express.json());
 
 // Routes
 const clientRoutes = require("./routes/clientRoutes");
-const freelancerRoutes = require("./routes/freelancerRoutes");
 
+const freelancerRoutes = require("./routes/freelancerRoutes");
 
 app.use("/api/client", clientRoutes);
 app.use("/api/freelancer", freelancerRoutes);
-
 
 // Start server
 app.listen(PORT, () => {
